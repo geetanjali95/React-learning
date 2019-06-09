50a65b83c524f583f692f4d9f3c663321111a617
shivanithakur04 <shivanithakur0404@gmail.com>
2019-06-09 12:12:18 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component{
+
+    commentObj=[{ author: 'Sam', date: 'Today, 6:00PM', text: 'Good Work Man!' }, { author: 'Sam', date: 'Today, 6:00PM', text: 'Good Work Man!' }];
+
+    renderDiv =(obj,index) =>{
+        return(
+        <div className='container' key={index}>
+            <div className='content'>
+                <div className='author'>{obj.author}</div>
+                <div className='date'>{obj.date}</div>
+                <div className='text'>{obj.text}</div>
+            </div>
+        </div>
+        );
+    }
+
+    render() {
+    return(
+        this.commentObj.map(
+            (data,index) =>{
+                return this.renderDiv(data,index);
+            }
+        )
+    )
+}
+}
+
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+);
